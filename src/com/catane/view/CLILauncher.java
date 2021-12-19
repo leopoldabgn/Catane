8fd1f3343eac8bbacaaa4488403e3b66e2f2f868
Leopold Abignoli <leopold.abignoli@gmail.com>
2021-12-20 00:18:01 +0100

    maj clilauncher

--- src/com/catane/view/CLILauncher.java
+++ src/com/catane/view/CLILauncher.java
@@ -5,7 +5,7 @@ import com.catane.model.Game;
 public class CLILauncher {
 
 	public static void main(String[] args) {
-		String jouer = "gui";
+		String jouer = "command";
 		if(args != null)
 			if(args.length > 0)
 				if(args[0].toUpperCase().equals("GUI"))
